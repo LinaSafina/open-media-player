04f7a4f80ccf3fd304d3e431b67e9140189d2f63
Alina Safina <lina.zumba@mail.ru>
2023-03-14 16:39:00 +0300

    fix svg not being shown in img tag

--- src/components/audio-player/audio-player.component.jsx
+++ src/components/audio-player/audio-player.component.jsx
@@ -71,9 +71,29 @@ const AudioPlayer = () => {
           <div className='play-pause-container'>
             <button className='play-pause-button' onClick={togglePlay}>
               {isPlaying ? (
-                <img src='../../assets/Pause.svg' />
+                <svg
+                  width='40'
+                  height='40'
+                  viewBox='0 0 40 40'
+                  fill='none'
+                  xmlns='http://www.w3.org/2000/svg'
+                >
+                  <rect x='4' width='4' height='40' fill='white' />
+                  <rect x='32' width='4' height='40' fill='white' />
+                </svg>
               ) : (
-                <img src='../../assets/Play.svg' />
+                <svg
+                  width='40'
+                  height='40'
+                  viewBox='0 0 40 40'
+                  fill='none'
+                  xmlns='http://www.w3.org/2000/svg'
+                >
+                  <path
+                    d='M0 40V0H4.34286L40 18.7952V20.9639L4.34286 40H0Z'
+                    fill='white'
+                  />
+                </svg>
               )}
             </button>
           </div>
